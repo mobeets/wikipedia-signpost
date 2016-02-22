8303cc368a9d61e72bb6341ccef136cbb9b5125d
mobeets <mobeets@gmail.com>
2016-02-21 19:01:20 -0500

    init

--- scraper.py
+++ scraper.py
@@ -1,18 +1,27 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+import scraperwiki
+import lxml.html
+from BeautifulSoup import BeautifulSoup
+import urllib2
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
+
+# TAG = "/wiki/Wikipedia:Wikipedia_Signpost/2016"
+# URL0 = "https://en.wikipedia.org/wiki/Wikipedia:Wikipedia_Signpost/Archives/2016"
+# html = scraperwiki.scrape(URL0)
 # root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
+# vs_raw = [(x.text, x.get('href')) for x in root.cssselect("a")]
+# vs = [v for v in vs_raw if v[0] and v[1].count(TAG) > 0]
+
+BASEURL = "https://en.wikipedia.org"
+URL = "https://en.wikipedia.org/wiki/Wikipedia:Wikipedia_Signpost"
+html = urllib2.urlopen(URL).read()
+obj = BeautifulSoup(html)
+base = obj.findAll('div', id='mw-content-text')[0]
+vs = [{'name': x.span.text, 'url': BASEURL + x['href']} for x in base.findAll('a') if x['href'].count('/wiki/Wikipedia:Wikipedia_Signpost/20') and x.span]
+
+print 'Found {0} new items.'.format(len(vs))
 #
 # # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
+scraperwiki.sqlite.save(unique_keys=['name'], data=vs)
 #
 # # An arbitrary query against the database
 # scraperwiki.sql.select("* from data where 'name'='peter'")
